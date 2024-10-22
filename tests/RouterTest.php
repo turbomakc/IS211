2df1c080f42b8822141afcc8a8c6e416f0e21944
Студент 9 Кабинет 409 <409-9@coopteh.local>
2024-10-22 17:28:20 +0700

    лох

---
+++ tests/RouterTest.php
@@ -0,0 +1,26 @@
+<?php 
+namespace Test;
+
+use PHPUnit\Framework\TestCase;
+use Routers\Router;
+
+class RouterTest extends TestCase {
+    public function test_router() {
+        $router = new Router();
+        $html = $router->route( "http://localhost/orders" );
+        $pos= mb_strpos($html, "Создание заказа");
+        $this->assertNotFalse( $pos>=0);
+    }
+    public function test_router1() {
+        $router = new Router();
+        $html = $router->route( "http://localhost" );
+        $pos= mb_strpos($html, "Приглашаем в наше онлайн-кафе");
+        $this->assertNotFalse( $pos>=0);
+    }
+    public function test_router2() {
+        $router = new Router();
+        $html = $router->route( "http://localhost/products" );
+        $pos= mb_strpos($html, "Добавить в корзину");
+        $this->assertNotFalse( $pos>=0);
+    }
+}
